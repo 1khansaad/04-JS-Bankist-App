5a1f6c38d3cd30d8352015a785709eeb858bbea1
1khansaad <saadkhan8268@gmail.com>
2022-07-03 10:57:48 -0700

    created userName funtion and added movement html

--- script.js
+++ script.js
@@ -61,85 +61,69 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovement = function(movemnets){
+
+  movemnets.forEach(function(mov , index){
+    const type = mov < 0 ? 'withdrawal' : 'deposit'
+    const html = `
+     
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${index + 1} : ${type} deposit</div>
+          <div class="movements__value">${mov}€</div>
+        </div>
+     `
+
+     containerMovements.insertAdjacentHTML('afterbegin', html)
+  });
+};
+displayMovement(account1.movements)
+
+
+const username = function(accounts){
+  accounts.forEach(function(acc){
+    const userNameMethod = acc.owner.toLowerCase().split(' ').map(function(Ele){
+      return Ele[0];}).join('')
+    console.log(userNameMethod)
+  })
+}
+username(accounts)
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+// challenge 1
+// const jhon = [3, 5, 2, 12, 7]
+// const sam = [9, 16 ,6, 8, 3]
 
+// const checkDog = function(dataJhon , dataSam){
+// const jhonCopy = jhon.slice(1,3);
+// const collectedData = [...jhonCopy, ...sam];
+// collectedData.forEach(function(age, index, arr){
+//   const check = age >= 3 ? console.log(`Dog number ${index + 1} is an adult, and is ${arr.at(index)} Years old.`) : console.log(`dog number ${index + 1} is still a puppy`)
+// });
+// };
+// checkDog();
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-// for (const movement of movements){
-//   movement > 0 ? console.log(`you + ${movement}`) :  console.log(`you - ${Math.abs(movement)}`)
-// }
+// const indianRs = [2010, 5432, 6754, 7400]
 
-// console.log(`-----------------------`)
-
-// movements.forEach(function(movement){
-//   movement > 0 ? console.log(`you + ${movement}`) :  console.log(`you - ${Math.abs(movement)}`)
+// const indToUSD = indianRs.map(function(Rs){
+//   return `${Rs / 74 }`
 // })
+// console.log(indToUSD)
 
-// const obj =  {
-//   name : 'saad',
-//   lastName : 'khan',
-//   friends : ['x', 'y', 'z'],
-
-//   detail : function(x){
-//     console.log(`${this.name} ${this.lastName} has three friends but his best friend is called ${this.friends[0]} ${x}`)
-//     // console.log(`${object.name} ${object.lastName} has three friends but his best friend is called ${object.friends[0]}`)
-//   }
-// }
-
-// document.querySelector('.login__btn').addEventListener('click', obj.detail.bind(obj))
-
-
-
-const array = ['xx', 'YY']
- const obj =  {
-  name : 'saad',
-  lastName : 'khan',
-  x : function(frnd1, frnd2){
-    console.log(`${this.name} ${this.lastName} has tWO frnd ${frnd1} ${frnd2}`)
-  }
- }
-
- const fun = obj.x
-
- const obj1 =  {
-  name : 'chetan',
-  lastName : 'narhar',
-  
- }
-
- const obj2 =  {
-  name : 'sahil',
-  lastName : 'narhar',
-  
- }
-
- const obj3 =  {
-  name : 'sameer',
-  lastName : 'narhar',
-  
- }
-
-//  obj.x('xx', 'yy')
-//  fun.call(obj1, 'AA', 'BB')
-
-fun.apply(obj1, array)
-// fun.call(obj1, ...array)
-fun.apply(obj2, array)
-fun.apply(obj3, array)
-
-
-
-
+// const movementDiscription = account1.movements.map(function(Element, index){
+//   return `Movement ${index + 1} : You ${Element > 0 ? `deposited` : `withdrawed`} ${Math.abs(Element)}`
+// }) 
+// console.log(movementDiscription)
